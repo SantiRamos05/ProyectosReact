0ad8aa2009377377e011560151252d81f5344ff7
SantiRamos <santi050699@gmail.com>
2022-04-12 17:58:29 -0500

    Suma()

--- s5-detalles/src/components/Calculadora.jsx
+++ s5-detalles/src/components/Calculadora.jsx
@@ -1,19 +1,12 @@
 import { useState } from 'react';
 import NumberInput from './NumberInput';
-import Resultado from './Resultado';
+
 
 const Calculadora = () => {
-  const [suma, setSuma] = useState(0);
- 
   return (
     <div>
         <NumberInput />
-        <Resultado operacion='Suma' calculo={suma} />
-        <Resultado operacion='Resta' calculo={suma} />
-        <Resultado operacion='Multiplicacion' calculo={suma} />
-        <Resultado operacion='Division' calculo={suma} />
-        
-        
+     
     </div>
   )
 }
--- s5-detalles/src/components/NumberInput.jsx
+++ s5-detalles/src/components/NumberInput.jsx
@@ -1,20 +1,25 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import Resultado from './Resultado';
 
 const NumberInput = () => {
+    
     const [numeros, setNumeros] = useState({
         numero1: 0,
         numero2: 0,
     });
 
     const { numero1, numero2} = numeros;
-
+ 
       const handleChange = (e) => {
            setNumeros({
             ...numeros,  //COPIA EL ESTADO ACTUAL DEL NUMERO
             [e.target.name] : parseFloat(e.target.value),
            });
+
       }
+
+      const suma = () => numero1 +numero2
 
   return (
     <>
@@ -24,6 +29,11 @@ const NumberInput = () => {
         <label>
            Numero 2: <input name='numero2' value={numero2} onChange={handleChange} type="number" />
         </label>
+
+        <Resultado operacion='Suma' calculo={suma()} />
+        <Resultado operacion='Resta' calculo={suma} />
+        <Resultado operacion='Multiplicacion' calculo={suma} />
+        <Resultado operacion='Division' calculo={suma} />
     </>
   )
 }
