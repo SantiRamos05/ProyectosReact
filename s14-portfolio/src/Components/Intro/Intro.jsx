2116f833e29321b7a77d4902b055274c3f4cf373
SantiRamos <santi050699@gmail.com>
2022-08-09 11:31:33 -0500

    s14 Portfolio

---
+++ s14-portfolio/src/Components/Intro/Intro.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Intro.css'
+
+const Intro = () => {
+  return (
+    <div className="intro">
+        <div className="i-left">
+            <div className="i-name">
+                <span>Hola :) Soy</span>
+                <span>Santiago Ramos</span>
+                <span>Frontend Developer with high level of experience in web designing
+            and development, producting the Quality work</span>
+            </div>
+            <button className="button i-button">Ver mas</button>
+        </div>
+        <div className="i-right"></div>
+    </div>
+  )
+}
+
+export default Intro
